d59eaac023dbc75fab36a97ab6e9e8cd28330f7b
gatheluck <gatheluck@gmail.com>
2025-04-24 02:13:21 +0900

    feat: adjust parameters.

--- scripts/compute_r2.py
+++ scripts/compute_r2.py
@@ -3,9 +3,11 @@ import pathlib
 import numpy as np
 import timm
 import torch
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, RidgeCV
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 from torch.utils.data import DataLoader, Subset
@@ -22,12 +24,12 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("--model-name", default="resnetv2_50x1_bit.goog_distilled_in1k")
     p.add_argument("--nodes",
-                   default="stages.0,stages.2",
+                   default="stages.0,stages.1,stages.2,stages.3,head.global_pool",
                    help="Comma separated list of FX graph node names.")
     p.add_argument("--device", default="cuda")
-    p.add_argument("--num-samples", type=int, default=20000)
+    p.add_argument("--num-samples", type=int, default=10000)
     p.add_argument("--batch-size", type=int, default=256)
-    p.add_argument("--ridge-alpha", type=float, default=1e-3)
+    p.add_argument("--ridge-alpha", type=float, default=100)
     args = p.parse_args()
 
     device = torch.device(args.device)
@@ -53,11 +55,17 @@ if __name__ == "__main__":
     with torch.no_grad():
         x0, _ = next(iter(loader))
         out0  = feature_extractor(x0.to(device))
-        stage0_sample = out0["stages_0"].mean((-1, -2)).cpu()   # (bsz, C0)
-        stage2_sample = out0["stages_2"].mean((-1, -2)).cpu()   # (bsz, C2)
+        stage0_sample = out0["stages_0"].mean((-1, -2)).cpu()
+        stage1_sample = out0["stages_1"].mean((-1, -2)).cpu()
+        stage2_sample = out0["stages_2"].mean((-1, -2)).cpu()
+        stage3_sample = out0["stages_3"].mean((-1, -2)).cpu()
+        gp_sample = out0["head_global_pool"].flatten(1).cpu()
 
     feat0 = torch.empty((args.num_samples, stage0_sample.size(1)), dtype=torch.float32)
+    feat1 = torch.empty((args.num_samples, stage1_sample.size(1)), dtype=torch.float32)
     feat2 = torch.empty((args.num_samples, stage2_sample.size(1)), dtype=torch.float32)
+    feat3 = torch.empty((args.num_samples, stage3_sample.size(1)), dtype=torch.float32)
+    feat_gp = torch.empty((args.num_samples, gp_sample.size(1)), dtype=torch.float32)
 
     print("Extracting features...")
     with torch.no_grad():
@@ -66,14 +74,25 @@ if __name__ == "__main__":
             end = start + x.size(0)
             out = feature_extractor(x.to(device))
             feat0[start:end] = out["stages_0"].mean((-1,-2)).cpu()
+            feat1[start:end] = out["stages_1"].mean((-1,-2)).cpu()
             feat2[start:end] = out["stages_2"].mean((-1,-2)).cpu()
+            feat3[start:end] = out["stages_3"].mean((-1,-2)).cpu()
+            feat_gp[start:end] = out["head_global_pool"].flatten(1).cpu()
 
     print("feat0.shape", feat0.shape)
+    print("feat1.shape", feat1.shape)
     print("feat2.shape", feat2.shape)
+    print("feat3.shape", feat3.shape)
+    print("feat_gp.shape", feat_gp.shape)
 
     feat0_np = feat0.numpy()
+    feat1_np = feat1.numpy()
     feat2_np = feat2.numpy()
-    x_train, x_test, y_train, y_test = train_test_split(feat0_np, feat2_np, test_size=0.2, random_state=0)
+    feat3_np = feat3.numpy()
+    feat_gp_np = feat_gp.numpy()
+
+    z_index = 100
+    x_train, x_test, y_train, y_test = train_test_split(feat3_np, feat_gp_np[:, z_index], test_size=0.2, random_state=0)
 
     print("x_train.shape", x_train.shape)
     print("x_test.shape", x_test.shape)
@@ -81,8 +100,16 @@ if __name__ == "__main__":
     print("y_test.shape", y_test.shape)
 
     print("Fitting Ridge regression...")
-    reg = Ridge(alpha=args.ridge_alpha, fit_intercept=True)
-    reg.fit(x_train, y_train)
+    if False:
+        # Use RidgeCV to find the best alpha
+        alphas = np.logspace(-3, 4, 50) # 1e-3 ~ 1e4
+        reg = make_pipeline(StandardScaler(with_mean=True), RidgeCV(alphas=alphas, cv=5, fit_intercept=True))
+        reg.fit(x_train, y_train)
+        print("best alpha =", reg[-1].alpha_)
+    else:
+        # Use a fixed alpha
+        reg = make_pipeline(StandardScaler(with_mean=True), Ridge(alpha=args.ridge_alpha, fit_intercept=True))
+        reg.fit(x_train, y_train)
 
     r2 = r2_score(y_test, reg.predict(x_test), multioutput="variance_weighted")
     print(f"R^2 = {r2:.4f}")
